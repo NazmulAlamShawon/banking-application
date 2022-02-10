ad11a268a83357e1238d87c552374357f9880b4b
Nazmul Alam <shawonkst15@gmail.com>
2022-02-10 23:10:58 +0600

    add new file

---
+++ js/banking.js
@@ -0,0 +1,37 @@
+document.getElementById('deposit-buttton').addEventListener('click', function(){
+
+ const depositInput = document.getElementById('deposit-input');
+ const newDepositText = depositInput.value;
+ const newDepositAmount = parseFloat(newDepositText);
+ 
+// update  deposit total
+ const depositTotal = document.getElementById('deposit-total');
+
+ const previousDepositText = depositTotal.innerText;
+ const previousDepositAmount = parseFloat(previousDepositText);
+
+ const newDepositTotal = previousDepositAmount + newDepositAmount;
+ 
+
+ depositTotal.innerText = newDepositTotal;
+ //update ballance
+ const balanceTotal = document.getElementById('balance-total');
+ const balanceTotalText=balanceTotal.innerText;
+ const previousBalanceTotal =parseFloat(balanceTotalText);
+ const newBalanceTotal = previousBalanceTotal + newDepositAmount;
+ balanceTotal.innerText=newBalanceTotal;
+
+
+ //clear input 
+ depositInput.value = '';
+
+ 
+
+     
+})
+
+
+//handle withdraw
+
+
+
